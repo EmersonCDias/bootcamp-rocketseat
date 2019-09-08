c88f5ef2a1e0e82a1fa3d60cdd04f687e9289641
EmersonCDias <emerson_cdias@hotmail.com>
2019-09-08 13:43:38 -0300

    Front get avatar img through url

--- modulo02 - GoBarber/src/routes.js
+++ modulo02 - GoBarber/src/routes.js
@@ -2,10 +2,11 @@ import { Router } from 'express';
 import multer from 'multer';
 import multerConfig from './config/multer';
 
-import FileController from './app/controllers/FIleController';
-import UserController from './app/controllers/UserController';
-import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import SessionController from './app/controllers/SessionController';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -16,6 +17,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+routes.get('/providers', ProviderController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
