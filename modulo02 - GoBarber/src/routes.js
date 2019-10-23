699ccc33d5cf98ac9155f19e7c1183ad0457995c
EmersonCDias <emerson_cdias@hotmail.com>
2019-10-22 23:44:45 -0300

    Saving progress

--- modulo02 - GoBarber/src/routes.js
+++ modulo02 - GoBarber/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import authMiddleware from './app/middlewares/auth';
 
 import AppointmentController from './app/controllers/AppointmentController';
+import AvailableController from './app/controllers/AvailableController';
 import FileController from './app/controllers/FileController';
 import NotificationController from './app/controllers/NotificationsController';
 import ProviderController from './app/controllers/ProviderController';
@@ -23,6 +24,7 @@ routes.use(authMiddleware);
 routes.put('/users', UserController.update);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
