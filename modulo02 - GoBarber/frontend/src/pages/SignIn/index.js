6244899d31f8710e82c3e53d97e29d9a0d54d4dc
EmersonCDias <emerson_cdias@hotmail.com>
2019-11-24 19:37:12 -0300

    Style and Unform

--- modulo02 - GoBarber/frontend/src/pages/SignIn/index.js
+++ modulo02 - GoBarber/frontend/src/pages/SignIn/index.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import Logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={Logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <button type="submit">Logar</button>
+
+        <Link to="/register">Criar conta gratuita</Link>
+      </Form>
+    </>
+  );
 };
 
 export default SignIn;
--- modulo02 - GoBarber/frontend/src/pages/SignUp/index.js
+++ modulo02 - GoBarber/frontend/src/pages/SignUp/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+
+import Logo from '~/assets/logo.svg';
 
 // import { Container } from './styles';
 
-const SingUp = () => {
-  return <div>SingUp</div>;
+const SignUp = () => {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={Logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome completo" />
+        <Input name="email" type="email" placeholder="E-mail" />
+        <Input name="password" type="password" placeholder="Senha" />
+
+        <button type="submit">Criar conta</button>
+
+        <Link to="/">Já tenho login</Link>
+      </Form>
+    </>
+  );
 };
 
-export default SingUp;
+export default SignUp;
