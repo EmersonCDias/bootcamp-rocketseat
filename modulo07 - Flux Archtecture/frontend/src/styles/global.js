6fcc668907aaf006f763357c6a3508a8c8af3583
EmersonCDias <emerson_cdias@hotmail.com>
2019-11-24 22:25:00 -0300

    Global styles

---
+++ modulo07 - Flux Archtecture/frontend/src/styles/global.js
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from 'styled-components';
+
+import background from '../assets/images/background.svg';
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap');
+
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  *:focus {
+    outline: 0;
+  }
+
+  #root {
+    max-width: 1020px;
+    margin: 0 auto;
+    padding: 0 20px 50px;
+
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    background: #191920 url(${background}) no-repeat center top;
+  }
+
+  body, input, button {
+    font: 14px 'Roboto', sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
