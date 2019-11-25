45b876a07e8bf053527c9c28797d1f7caf164b2b
EmersonCDias <emerson_cdias@hotmail.com>
2019-11-24 22:44:28 -0300

    Creating header

---
+++ modulo07 - Flux Archtecture/frontend/src/components/Header/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { MdShoppingBasket } from 'react-icons/md';
+
+import { Container, Cart } from './styles';
+import Logo from '../../assets/images/logo.svg';
+
+const Header = () => {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={Logo} alt="RocketShoes" />
+      </Link>
+
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <strong>3 items</strong>
+        </div>
+
+        <MdShoppingBasket size={36} color="#fff" />
+      </Cart>
+    </Container>
+  );
+};
+
+export default Header;
