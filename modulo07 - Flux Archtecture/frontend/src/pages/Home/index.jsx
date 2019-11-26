a6db8641667cee4a22ed547be9d41b7d3eeef0c2
EmersonCDias <emerson_cdias@hotmail.com>
2019-11-26 00:41:53 -0300

    Creting api fake

--- modulo07 - Flux Archtecture/frontend/src/pages/Home/index.jsx
+++ modulo07 - Flux Archtecture/frontend/src/pages/Home/index.jsx
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-const Home = () => {
-  return <div>Home</div>;
+import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../util/format';
+
+export default class Home extends Component {
+  state = {
+    products: [],
+  }
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(item => ({
+      ...item,
+      priceFormatted: formatPrice(item.price),
+    }));
+
+    this.setState({
+      products: data,
+    })
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(({ id, title, priceFormatted, image }) => (
+          <li key={id}>
+            <img src={image} alt={title} />
+
+            <strong>{title}</strong>
+
+            <span>{priceFormatted}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#fff" /> 3
+              </div>
+
+              <span>ADICIONAR AO CARRINHO</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 };
-
-export default Home;
